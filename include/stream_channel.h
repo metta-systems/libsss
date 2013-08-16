a3a38227f55330c66f98135b355dc25715f84b39
Berkus <berkus@gmail.com>
2013-08-16 23:46:28 +0200

    More methods for stream_channel.

--- include/stream_channel.h
+++ include/stream_channel.h
@@ -18,10 +18,18 @@ namespace ssu {
 class stream_channel : public channel
 {
     stream_peer* peer_;
-    queue<base_stream*> tx_streams;
+
+    /**
+     * Streams queued for transmission on this channel.
+     * This should be a priority queue for simplicity of enqueueing.
+     */
+    priority_queue<base_stream*> tx_streams;
+
+    std::unordered_map<tx_seq_id, packet*> waiting_ack_;
+    std::unordered_map<tx_seq_id, packet*> waiting_expiry_;
 
 public:
-    stream_channel(Host *h, StreamPeer *peer, const PeerId &peerid);
+    stream_channel(std::shared_ptr<host> host, stream_peer* peer, const peer_id& id);
     ~stream_channel();
 
     inline stream_peer* target_peer() { return peer_; }
@@ -36,8 +44,14 @@ public:
     void detach_all();
 
 
-    virtual void start(bool initiate) override;
-    virtual void stop() override;
+    void start(bool initiate) override;
+    void stop() override;
+
+    bool transmit_ack(byte_array &pkt, uint64_t ackseq, unsigned ackct) override;
+
+    void acknowledged(quint64 txseq, int npackets, quint64 rxackseq) override;
+    void missed(quint64 txseq, int npackets) override;
+    void expire(quint64 txseq, int npackets) override;
 };
 
 } // namespace ssu
