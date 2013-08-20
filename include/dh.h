b6e45f81f28861ec9de81a61dab7433348a51483
Berkus <berkus@gmail.com>
2013-08-20 22:01:55 +0200

    Add Diffie-Hellman host key negotiation support.

--- include/dh.h
+++ include/dh.h
@@ -1,29 +1,70 @@
 #pragma once
 
 #include "byte_array.h"
+#include "timer.h"
 #include "negotiation/key_message.h"
+#include <openssl/dh.h>
 
 namespace ssu {
+
+class host;
+class dh_host_state;
+
 namespace negotiation {
 
-class dh_hostkey_t
-{
-public:
-    byte_array public_key;
-    byte_array hkr; // hmac secret key
+class key_responder;
+class key_initiator;
 
-    //temporary for testing
-    dh_hostkey_t() { public_key.resize(256); hkr.resize(32); }
+class dh_hostkey_t : public std::enable_shared_from_this<dh_hostkey_t>
+{
+    friend class ssu::dh_host_state;
+    friend class ssu::negotiation::key_responder; // @fixme remove
+    friend class ssu::negotiation::key_initiator; // @fixme remove
+
+    std::shared_ptr<ssu::host> host_;    // Host to which this key is attached.
+    ssu::async::timer expiration_timer_; // Expiration timer for DH master key.
+    negotiation::dh_group_type group_;
+    DH *dh_;
+    byte_array public_key_; // Host's public key.
+    byte_array hmac_secret_key_; // HMAC key for responder's challenge.
+
+    /**
+     * Hash table of cached R2 responses made using this key, for replay protection.
+     */
+    std::map<byte_array, byte_array> r2_cache_;
+
+    dh_hostkey_t(std::shared_ptr<ssu::host> host, negotiation::dh_group_type group, DH *dh);
+
+    /**
+     * Expire the key.
+     */
+    void timeout();
+
+    /**
+     * Compute a shared master secret from our private key and other_public_key.
+     */
+    byte_array calc_key(byte_array const& other_public_key);
 };
 
 } // namespace negotiation
 
 class dh_host_state
 {
+    friend class negotiation::dh_hostkey_t; // @fixme remove
+
+    std::shared_ptr<negotiation::dh_hostkey_t>
+    dh_keys_[/*dh_group_max*/3];
+
+    std::shared_ptr<negotiation::dh_hostkey_t>
+    _generate_dh_key(negotiation::dh_group_type group, DH *(*groupfunc)());
+
 public:
-    std::shared_ptr<negotiation::dh_hostkey_t> get_dh_key(negotiation::dh_group_type group) {
-        return std::make_shared<negotiation::dh_hostkey_t>(); //@fixme temporary
-    }
+    dh_host_state();
+    virtual ~dh_host_state();
+
+    std::shared_ptr<negotiation::dh_hostkey_t> get_dh_key(negotiation::dh_group_type group);
+
+    virtual std::shared_ptr<host> get_host() = 0;
 };
 
 } // namespace ssu
