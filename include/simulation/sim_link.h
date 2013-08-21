ee44e366d8ee2296b21a93e6a265ce24c5f4df58
Berkus <berkus@gmail.com>
2013-08-21 23:34:13 +0200

    Implement more of sim_link.

--- include/simulation/sim_link.h
+++ include/simulation/sim_link.h
@@ -1,6 +1,41 @@
-// SimSocket
+//
+// Part of Metta OS. Check http://metta.exquance.com for latest version.
+//
+// Copyright 2007 - 2013, Stanislav Karchebnyy <berkus@exquance.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#pragma once
+
+#include "link.h"
+
 namespace ssu {
+
+class host;
+
 namespace simulation {
+
+class sim_host;
+class simulator;
+
+class sim_link : public link
+{
+    std::shared_ptr<simulator> simulator_;
+    std::shared_ptr<sim_host> host_;
+    uint16_t port_{0};
+
+public:
+    sim_link(std::shared_ptr<host> host);
+    ~sim_link();
+
+    bool send(const endpoint& ep, const char *data, size_t size) override;
+
+    // void bind() override;
+    // void unbind() override;
+
+    std::vector<endpoint> local_endpoints() override;
+};
 
 } // simulation namespace
 } // ssu namespace
---
+++ lib/simulation/sim_link.cpp
@@ -0,0 +1,47 @@
+#include "simulation/sim_link.h"
+#include "simulation/sim_host.h"
+#include "simulation/sim_packet.h"
+
+namespace ssu {
+namespace simulation {
+
+sim_link::sim_link(std::shared_ptr<host> host)
+    : link(*host)
+    , host_(host)
+    , simulator_(host->get_simulator())
+{}
+
+sim_link::~sim_link()
+{
+    // unbind();
+}
+
+// Target address must be routable to in order to send.
+// Find the destination host in the "routing table".
+bool sim_link::send(const endpoint& ep, const char *data, size_t size)
+{
+    assert(port_ > 0);
+
+    endpoint src;
+    src.port(port_);
+    std::shared_ptr<sim_host> dest_host = host_->neighbor_at(ep, src);
+    if (!dest_host) {
+        logger::warning() << "Unknown or non-adjacent target host " << ep;
+        return false;
+    }
+
+    std::shared_ptr<sim_connection> pipe(host_->connection_at(src));
+    assert(pipe);
+
+    new sim_packet(host_, src, pipe, ep, byte_array(data, size)); //@todo keep reference around
+    return true;
+}
+
+std::vector<endpoint>
+sim_link::local_endpoints()
+{
+    return std::vector<endpoint>();
+}
+
+} // simulation namespace
+} // ssu namespace
