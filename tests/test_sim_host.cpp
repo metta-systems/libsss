11f148b4a9dd986f4c04f824f25d213d912c83d3
Berkus <berkus@gmail.com>
2013-08-23 21:24:19 +0200

    Add test for sim_host.

---
+++ tests/test_sim_host.cpp
@@ -0,0 +1,23 @@
+//
+// Part of Metta OS. Check http://metta.exquance.com for latest version.
+//
+// Copyright 2007 - 2013, Stanislav Karchebnyy <berkus@exquance.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#define BOOST_TEST_MODULE Test_sim_host
+#include <boost/test/unit_test.hpp>
+
+#include "simulation/simulator.h"
+#include "simulation/sim_host.h"
+
+using namespace std;
+using namespace ssu;
+using namespace ssu::simulation;
+
+BOOST_AUTO_TEST_CASE(created_host)
+{
+    shared_ptr<simulator> sim(make_shared<simulator>());
+    shared_ptr<sim_host> my_host(make_shared<sim_host>(sim));
+}
