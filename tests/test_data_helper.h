17cd8912bc56858f2a774119b91911f81b62ff02
Berkus <berkus@gmail.com>
2013-08-25 20:23:11 +0200

    Add helper to generate key messages.

---
+++ tests/test_data_helper.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <boost/archive/binary_oarchive.hpp>
+#include "byte_array.h"
+#include "protocol.h"
+#include "negotiation/key_message.h"
+
+/**
+ * Generate a binary blob for testing key_message.h I/O functions.
+ * Create a key message with dh_init1 and checksum_init chunks, add packet chunk of data afterwards.
+ *
+ * +-------+--------+-----------------------------------------------------------------+
+ * | magic | count  | opaque                                                          |
+ * +-------+--------+--------+-------+------------------------------------------------+
+ *                  | length | discr |                                                |
+ *                  +--------+-------+------------------------------------------------+
+ *                     0xb4    0x21  | DHgroup | keymin | nhi | length | dhi | length |
+ *                                   +---------+--------+-----+--------+-----+--------+
+ *                                       0x1      0x10    32b
+ */
+inline byte_array
+generate_dh1_chunk()
+{
+    byte_array data;
+    {
+        boost::iostreams::filtering_ostream out(boost::iostreams::back_inserter(data.as_vector()));
+        boost::archive::binary_oarchive oa(out, boost::archive::no_header);
+
+        ssu::negotiation::key_message m;
+        ssu::negotiation::key_chunk chu;
+        ssu::negotiation::dh_init1_chunk dh;
+
+        m.magic = ssu::stream_protocol::magic;
+        chu.type = ssu::negotiation::key_chunk_type::dh_init1;
+        dh.group = ssu::negotiation::dh_group_type::dh_group_1024;
+        dh.key_min_length = 0x10;
+
+        dh.initiator_hashed_nonce.resize(32);
+        for (int i = 0; i < 32; ++i)
+            dh.initiator_hashed_nonce[i] = rand();
+        dh.initiator_dh_public_key.resize(128);
+        for (int i = 0; i < 128; ++i)
+            dh.initiator_dh_public_key[i] = 255 - i;
+
+        chu.dh_init1 = dh;
+
+        m.chunks.push_back(chu);
+        oa << m;
+    }
+    return data;
+}
--- tests/test_serialization.cpp
+++ tests/test_serialization.cpp
@@ -8,7 +8,6 @@
 //
 #include <fstream>
 #include <boost/archive/binary_iarchive.hpp>
-#include <boost/archive/binary_oarchive.hpp>
 #include <boost/optional/optional.hpp>
 #include "protocol.h"
 #include "byte_array.h"
@@ -19,50 +18,14 @@
 #include <boost/test/unit_test.hpp>
 
 #include "link.h"
+#include "test_data_helper.h"
 
 using namespace std;
 
-/**
- * Generate a binary blob for testing key_message.h i/o functions.
- * Create a key message with dh_init1 and checksum_init chunks, add packet chunk of data afterwards.
- *
- * +-------+--------+-----------------------------------------------------------------+
- * | magic | count  | opaque                                                          |
- * +-------+--------+--------+-------+------------------------------------------------+
- *                  | length | discr |                                                |
- *                  +--------+-------+------------------------------------------------+
- *                     0xb4    0x21  | DHgroup | keymin | nhi | length | dhi | length |
- *                                   +---------+--------+-----+--------+-----+--------+
- *                                       0x1      0x10    32b
- */
 BOOST_AUTO_TEST_CASE(serialize_and_deserialize)
 {
-    byte_array data;
-    {
-        boost::iostreams::filtering_ostream out(boost::iostreams::back_inserter(data.as_vector()));
-        boost::archive::binary_oarchive oa(out, boost::archive::no_header);
+    byte_array data = generate_dh1_chunk();
 
-        ssu::negotiation::key_message m;
-        ssu::negotiation::key_chunk chu;
-        ssu::negotiation::dh_init1_chunk dh;
-
-        m.magic = ssu::stream_protocol::magic;
-        chu.type = ssu::negotiation::key_chunk_type::dh_init1;
-        dh.group = ssu::negotiation::dh_group_type::dh_group_1024;
-        dh.key_min_length = 0x10;
-
-        dh.initiator_hashed_nonce.resize(32);
-        for (int i = 0; i < 32; ++i)
-            dh.initiator_hashed_nonce[i] = rand();
-        dh.initiator_dh_public_key.resize(128);
-        for (int i = 0; i < 128; ++i)
-            dh.initiator_dh_public_key[i] = 255 - i;
-
-        chu.dh_init1 = dh;
-
-        m.chunks.push_back(chu);
-        oa << m;
-    }
     {
         boost::iostreams::filtering_istream in(boost::make_iterator_range(data.as_vector()));
         boost::archive::binary_iarchive ia(in, boost::archive::no_header);
