30661bad79950a0a33cca6523b8efe2c467d5a85
Berkus <berkus@gmail.com>
2013-08-27 23:05:06 +0200

    Add RSA160 signing key type.

---
+++ include/crypto/rsa160_key.h
@@ -0,0 +1,43 @@
+//
+// Part of Metta OS. Check http://metta.exquance.com for latest version.
+//
+// Copyright 2007 - 2013, Stanislav Karchebnyy <berkus@exquance.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#pragma once
+
+#include "sign_key.h"
+#include <openssl/rsa.h>
+
+namespace ssu {
+namespace crypto {
+
+class rsa160_key : public sign_key
+{
+    RSA* rsa_;
+
+    rsa160_key(RSA* rsa);
+
+public:
+    rsa160_key(byte_array const& key);
+    rsa160_key(int bits = 0, unsigned e = 65537);
+    ~rsa160_key();
+
+    byte_array id() const override;
+
+    byte_array public_key() const override;
+    byte_array private_key() const override;
+
+    std::unique_ptr<secure_hash> create_hash() const override;
+
+    byte_array sign(byte_array const& digest) const override;
+    bool verify(byte_array const& digest, byte_array const& signature) const override;
+
+private:
+    void dump() const;
+};
+
+} // crypto namespace
+} // ssu namespace
---
+++ lib/crypto/rsa160_key.cpp
@@ -0,0 +1,137 @@
+//
+// Part of Metta OS. Check http://metta.exquance.com for latest version.
+//
+// Copyright 2007 - 2013, Stanislav Karchebnyy <berkus@exquance.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// #include <openssl/err.h>
+// #include <openssl/obj_mac.h>
+#include <boost/iostreams/filtering_stream.hpp>
+#include <boost/iostreams/device/back_inserter.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <openssl/sha.h>
+#include "crypto/rsa160_key.h"
+#include "crypto/utils.h"
+#include "crypto.h"
+#include "byte_array.h"
+#include "msgpack_archive.h"
+
+namespace ssu {
+namespace crypto {
+
+rsa160_key::rsa160_key(RSA *rsa)
+    : rsa_(rsa)
+{}
+
+/// @todo
+rsa160_key::rsa160_key(byte_array const& key)
+{}
+
+rsa160_key::rsa160_key(int bits, unsigned e)
+{
+    if (bits == 0) {
+        bits = 1024;
+    }
+    if (e == 0) {
+        e = 65537;
+    }
+    if (!(e % 2)) {
+        ++e; // e must be odd
+    }
+
+    // Generate a new RSA key given those parameters
+    rsa_ = RSA_generate_key(bits, e, NULL, NULL);
+
+    assert(rsa_);
+    assert(rsa_->d);
+
+    set_type(public_and_private);
+}
+
+rsa160_key::~rsa160_key()
+{
+    if (rsa_) {
+        RSA_free(rsa_);
+        rsa_ = NULL;
+    }
+}
+
+byte_array 
+rsa160_key::id() const
+{
+    if (type() == invalid)
+        return byte_array();
+
+    crypto::hash::value hash;
+    crypto::hash md;
+    md.update(public_key().as_vector()).finalize(hash);
+
+    byte_array id(hash);
+    // Only return 160 bits of key identity information,
+    // because this method's security may be limited by the SHA-1 hash
+    // used in the RSA-OAEP padding process.
+    id.resize(160/8);
+
+    return id;
+}
+
+byte_array
+rsa160_key::public_key() const
+{
+    byte_array data;
+    {
+        boost::iostreams::filtering_ostream out(boost::iostreams::back_inserter(data.as_vector()));
+        msgpack_oarchive oa(out, boost::archive::no_header);
+
+        // Write the public part of the key
+        // bool has_private_key = false;
+        oa << crypto::utils::bn2ba(rsa_->n) << rsa_->e << false;
+    }
+    return data;
+}
+
+byte_array
+rsa160_key::private_key() const
+{
+    byte_array data;
+    {
+    }
+    return data;
+}
+
+struct secure_hash {}; // @fixme temp
+
+std::unique_ptr<secure_hash>
+rsa160_key::create_hash() const
+{
+    return std::unique_ptr<secure_hash>(nullptr); //new sha256_hash();
+}
+
+byte_array
+rsa160_key::sign(byte_array const& digest) const
+{
+    if (type() == invalid)
+        return byte_array();
+    assert(digest.size() == SHA256_DIGEST_LENGTH);
+
+    byte_array signature;
+    unsigned siglen = RSA_size(rsa_);
+    signature.resize(siglen);
+
+    return signature;
+}
+
+bool
+rsa160_key::verify(byte_array const& digest, byte_array const& signature) const
+{
+    return false;
+}
+
+void
+rsa160_key::dump() const
+{}
+
+} // crypto namespace
+} // ssu namespace
