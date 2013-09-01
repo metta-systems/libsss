ed83c5411ddad4a2d51acfa12b624d2a4e32c83e
Berkus <berkus@gmail.com>
2013-09-01 23:37:02 +0300

    Add more specializations for msgpack.

--- include/msgpack_specializations.h
+++ include/msgpack_specializations.h
@@ -1,8 +1,21 @@
+#pragma once
+
 #include "msgpack.hpp"
 #include "byte_array.h"
 #include <boost/optional/optional.hpp>
+#include <boost/array.hpp>
 
 namespace msgpack {
+
+// Specialization for enum types.
+template <typename Stream, typename T>
+inline packer<Stream>& operator << (packer<Stream>& o,
+    typename std::enable_if<std::is_enum<T>::value, T>::type& v)
+{
+    o << to_underlying(v);
+    return o;
+}
+
 
 // Specialization for byte_array.
 template <typename Stream>
@@ -22,6 +35,27 @@ inline packer<Stream>& operator << (packer<Stream>& o, const boost::optional<T>&
     } else {
         o.pack_nil();
     }
+    return o;
+}
+
+// Specialization for boost::array<T,N>
+template <typename Stream, typename T, size_t N>
+inline packer<Stream>& operator << (packer<Stream>& o, const boost::array<T,N>& v)
+{
+    o.pack_array(v.size());
+    for(typename std::vector<T>::const_iterator it(v.begin()), it_end(v.end());
+            it != it_end; ++it) {
+        o.pack(*it);
+    }
+    return o;
+}
+
+// Specialization for boost::array<unsigned char,N>
+template <typename Stream, size_t N>
+inline packer<Stream>& operator << (packer<Stream>& o, const boost::array<unsigned char,N>& v)
+{
+    o.pack_array(v.size());
+    o.pack_raw_body(v.data(), v.size());
     return o;
 }
 
