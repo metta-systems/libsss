bbf7954217e03034068b5d1571ada7d33f2b629c
Berkus <berkus@gmail.com>
2013-09-03 09:51:35 +0300

    Convert serialization test to msgpack.

--- tests/test_serialization.cpp
+++ tests/test_serialization.cpp
@@ -7,31 +7,33 @@
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 #include <fstream>
-#include <boost/archive/binary_iarchive.hpp>
-#include <boost/optional/optional.hpp>
 #include "protocol.h"
 #include "byte_array.h"
+#include "msgpack_ostream.h"
+#include "msgpack_specializations.h"
+#include "msgpack_iarchive.h"
 #include "custom_optional.h"
-#include "msgpack.h"
 #include "negotiation/key_message.h"
 #define BOOST_TEST_MODULE Test_key_message_serialization
 #include <boost/test/unit_test.hpp>
 
 #include "link.h"
 #include "test_data_helper.h"
+#include "logging.h"
 
 using namespace std;
 
-BOOST_AUTO_TEST_CASE(serialize_msgpack_integer)
+BOOST_AUTO_TEST_CASE(serialize_msgpack_types)
 {
     byte_array data;
     {
-        byte_array_owrap<msgpack_oarchive> write(data);
+        byte_array_owrap<msgpack_ostream> write(data);
         uint64_t m = 42;
         uint64_t p = 0xdeadbeefabba;
         bool t = false;
         bool f = true;
-        write.archive() << t << f;
+        byte_array data{'a','b','c','d','e'};
+        write.archive() << t << f << m << p << data;
     }
     logger::file_dump out(data);
 }
@@ -45,12 +47,12 @@ BOOST_AUTO_TEST_CASE(serialize_msgpack_integer)
 //     }
 
 //     {
-//         byte_array_iwrap<boost::archive::binary_iarchive> r(data);
 //         ssu::negotiation::key_message m;
+//         byte_array_iwrap<boost::archive::binary_iarchive> read(data);
 
-//         BOOST_CHECK(data.size() == 188);
+//         BOOST_CHECK(data.size() == 0xbf);
 
-//         r.archive() >> m;
+//         read.archive() >> m;
 
 //         BOOST_CHECK(m.magic == ssu::stream_protocol::magic);
 //         BOOST_CHECK(m.chunks.size() == 1);
