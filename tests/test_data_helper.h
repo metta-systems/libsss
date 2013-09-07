17fc6590e6dd982b21f388891bb96cadb980411e
Berkus <berkus@gmail.com>
2013-09-08 00:52:26 +0300

    Add one more test chunk.

--- tests/test_data_helper.h
+++ tests/test_data_helper.h
@@ -9,15 +9,6 @@
 /**
  * Generate a binary blob for testing key_message.h I/O functions.
  * Create a key message with dh_init1 and checksum_init chunks, add packet chunk of data afterwards.
- *
- * +-------+--------+-----------------------------------------------------------------+
- * | magic | count  | opaque                                                          |
- * +-------+--------+--------+-------+------------------------------------------------+
- *                  | length | discr |                                                |
- *                  +--------+-------+------------------------------------------------+
- *                     0xb4    0x21  | DHgroup | keymin | nhi | length | dhi | length |
- *                                   +---------+--------+-----+--------+-----+--------+
- *                                       0x1      0x10    32b
  */
 inline byte_array
 generate_dh1_chunk()
@@ -25,8 +16,9 @@ generate_dh1_chunk()
     byte_array data;
     {
         ssu::negotiation::key_message m;
-        ssu::negotiation::key_chunk chu;
+        ssu::negotiation::key_chunk chu, chu2;
         ssu::negotiation::dh_init1_chunk dh;
+        ssu::negotiation::checksum_init_chunk cic;
 
         m.magic = ssu::stream_protocol::magic;
         chu.type = ssu::negotiation::key_chunk_type::dh_init1;
@@ -35,7 +27,7 @@ generate_dh1_chunk()
 
         dh.initiator_hashed_nonce.resize(32);
         for (int i = 0; i < 32; ++i)
-            dh.initiator_hashed_nonce[i] = rand();
+            dh.initiator_hashed_nonce[i] = i;
         dh.initiator_dh_public_key.resize(128);
         for (int i = 0; i < 128; ++i)
             dh.initiator_dh_public_key[i] = 255 - i;
@@ -43,6 +35,11 @@ generate_dh1_chunk()
         chu.dh_init1 = dh;
 
         m.chunks.push_back(chu);
+
+        chu2.type = ssu::negotiation::key_chunk_type::checksum_init;
+        chu2.checksum_init = cic;
+
+        m.chunks.push_back(chu2);
 
         byte_array_owrap<flurry::oarchive> write(data);
         write.archive() << m;
