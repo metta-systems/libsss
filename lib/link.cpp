097837927ef95eb7b8db4a2a7923c7c0866a3c05
Berkus <berkus@gmail.com>
2013-09-13 00:40:16 +0300

    Source cleanup.

--- lib/link.cpp
+++ lib/link.cpp
@@ -15,7 +15,12 @@
 
 namespace ssu {
 
-link_receiver* link_host_state::receiver(magic_t magic)
+//=================================================================================================
+// link_host_state
+//=================================================================================================
+
+link_receiver*
+link_host_state::receiver(magic_t magic)
 {
     auto it = receivers.find(magic);
     if (it == receivers.end())
@@ -26,7 +31,12 @@ link_receiver* link_host_state::receiver(magic_t magic)
     return it->second;
 }
 
-bool link_endpoint::send(const char *data, int size) const
+//=================================================================================================
+// link_endpoint
+//=================================================================================================
+
+bool
+link_endpoint::send(const char *data, int size) const
 {
     if (auto l = link_/*.lock()*/)
     {
@@ -35,6 +45,10 @@ bool link_endpoint::send(const char *data, int size) const
     logger::debug() << "Trying to send on a nonexistent socket";
     return false;
 }
+
+//=================================================================================================
+// link
+//=================================================================================================
 
 link::~link()
 {
@@ -74,7 +88,8 @@ link::~link()
  *  +--------------------------------------------------+
  *
  */
-void link::receive(const byte_array& msg, const link_endpoint& src)
+void
+link::receive(const byte_array& msg, const link_endpoint& src)
 {
     if (msg.size() < 4)
     {
@@ -133,6 +148,10 @@ link::unbind_channel(endpoint const& ep, channel_number chan)
     channels_.erase(std::make_pair(ep, chan));
 }
 
+//=================================================================================================
+// udp_link
+//=================================================================================================
+
 udp_link::udp_link(const endpoint& ep, link_host_state& h)
     : link(h)
     , udp_socket(h.get_io_service())
@@ -141,7 +160,8 @@ udp_link::udp_link(const endpoint& ep, link_host_state& h)
     bind(ep); // temporarily maintain old behaviour where creating udp_link binds it
 }
 
-void udp_link::prepare_async_receive()
+void
+udp_link::prepare_async_receive()
 {
     boost::asio::streambuf::mutable_buffers_type buffer = received_buffer.prepare(2048);
     udp_socket.async_receive_from(
@@ -158,7 +178,8 @@ udp_link::local_endpoints()
     return {udp_socket.local_endpoint()};
 }
 
-bool udp_link::bind(endpoint const& ep)
+bool
+udp_link::bind(endpoint const& ep)
 {
     boost::system::error_code ec;
     udp_socket.open(ep.protocol(), ec);
@@ -177,19 +198,22 @@ bool udp_link::bind(endpoint const& ep)
     return true;
 }
 
-void udp_link::unbind()
+void
+udp_link::unbind()
 {
     udp_socket.shutdown(boost::asio::ip::udp::socket::shutdown_both);
     udp_socket.close();
     set_active(false);
 }
 
-bool udp_link::send(const endpoint& ep, const char *data, size_t size)
+bool
+udp_link::send(const endpoint& ep, const char *data, size_t size)
 {
     return udp_socket.send_to(boost::asio::buffer(data, size), ep);
 }
 
-void udp_link::udp_ready_read(const boost::system::error_code& error, std::size_t bytes_transferred)
+void
+udp_link::udp_ready_read(const boost::system::error_code& error, std::size_t bytes_transferred)
 {
     if (!error)
     {
