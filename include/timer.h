d68a66915f1f404211d7e8540126da2abb334359
Berkus <berkus@gmail.com>
2013-09-30 16:01:34 +0300

    Change indent style.

--- include/timer.h
+++ include/timer.h
@@ -26,41 +26,42 @@ namespace async {
 class timer
 {
 public:
-	typedef boost::asio::deadline_timer::duration_type duration_type;
+    typedef boost::asio::deadline_timer::duration_type duration_type;
 
-	static const duration_type retry_min;
-	static const duration_type retry_max;
-	static const duration_type fail_max;
+    static const duration_type retry_min;
+    static const duration_type retry_max;
+    static const duration_type fail_max;
 
 private:
-	std::unique_ptr<timer_engine> engine_{nullptr};
-	duration_type interval_{retry_min};
-	boost::asio::deadline_timer::time_type deadline_;
-	bool active_{false};
+    std::unique_ptr<timer_engine> engine_{nullptr};
+    duration_type interval_{retry_min};
+    // boost::asio::deadline_timer::time_type deadline_;
+    bool active_{false};
 
 public:
-	timer(ssu::timer_host_state* host);
-
 	void start(duration_type interval); // @todo: support deadline timers too
-	void stop();
-	void restart();
 	bool has_failed() const;
+    timer(ssu::timer_host_state* host);
 
-	inline bool is_active() const {
-		return active_;
-	}
-	inline duration_type interval() const {
-		return interval_;
-	}
+    void stop();
+    void restart();
 
-	/**
-	 * Signaled when the timer expires.
-	 *
-	 * timeout signal handler signature is
-	 * void timeout(bool failed)
-	 * 
-	 * Argument 'failed' is true if the hard failure deadline has been reached.
-	 */
+    inline bool is_active() const {
+        return active_;
+    }
+    inline duration_type interval() const {
+        return interval_;
+    }
+
+
+    /**
+     * Signaled when the timer expires.
+     *
+     * timeout signal handler signature is
+     * void timeout(bool failed)
+     * 
+     * Argument 'failed' is true if the hard failure deadline has been reached.
+     */
     typedef boost::signals2::signal<void (bool)> timeout_signal;
     timeout_signal on_timeout;
 };
@@ -70,17 +71,17 @@ public:
 class timer_host_state : virtual public asio_host_state
 {
 public:
-	/**
-	 * Get current time. Can be virtualized to provide simulated time in testing environments.
-	 * @return Current time in a boost.asio-defined time type.
-	 */
-	virtual boost::posix_time::ptime current_time();
-	/**
-	 * Create a timer engine to perform various delays. Can be virtualized to simulate delays.
-	 * @param  t Timer to attach the engine to.
-	 * @return   New copy of a timer engine.
-	 */
-	virtual std::unique_ptr<async::timer_engine> create_timer_engine_for(async::timer* t);
+    /**
+     * Get current time. Can be virtualized to provide simulated time in testing environments.
+     * @return Current time in a boost.asio-defined time type.
+     */
+    virtual boost::posix_time::ptime current_time();
+    /**
+     * Create a timer engine to perform various delays. Can be virtualized to simulate delays.
+     * @param  t Timer to attach the engine to.
+     * @return   New copy of a timer engine.
+     */
+    virtual std::unique_ptr<async::timer_engine> create_timer_engine_for(async::timer* t);
 };
 
 } // namespace ssu
