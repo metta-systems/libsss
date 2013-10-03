43f4f15975bdae57714659f499d1b4689776a82a
Berkus <berkus@gmail.com>
2013-10-03 20:25:12 +0300

    Replace at() with non-throwing version.

--- lib/base_stream.cpp
+++ lib/base_stream.cpp
@@ -76,8 +76,11 @@ void base_stream::clear()
     // De-register us from our peer
     if (peer_)
     {
-        if (peer_->usid_streams_.at(usid_) == this) //@fixme at() may throw...
-            peer_->usid_streams_.erase(usid_);
+        if (contains(peer_->usid_streams_, usid_)) {
+            if (peer_->usid_streams_[usid_] == this) {
+                peer_->usid_streams_.erase(usid_);
+            }
+        }
         peer_->all_streams_.erase(this);
         peer_ = nullptr;
     }
