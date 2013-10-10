a204da52af1e249e3bf220e72582e63aa5b71501
Berkus <berkus@gmail.com>
2013-10-10 15:24:19 +0300

    Add identity tests stub.

---
+++ tests/test_identity.cpp
@@ -0,0 +1,31 @@
+//
+// Part of Metta OS. Check http://metta.exquance.com for latest version.
+//
+// Copyright 2007 - 2013, Stanislav Karchebnyy <berkus@exquance.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#define BOOST_TEST_MODULE Test_identity
+#include <boost/test/unit_test.hpp>
+
+#include "identity.h"
+
+using namespace std;
+using namespace ssu;
+
+BOOST_AUTO_TEST_CASE(null_identity)
+{
+}
+
+BOOST_AUTO_TEST_CASE(mac_identity)
+{
+}
+
+BOOST_AUTO_TEST_CASE(ip_identity)
+{
+}
+
+BOOST_AUTO_TEST_CASE(endpoint_identity)
+{
+}
