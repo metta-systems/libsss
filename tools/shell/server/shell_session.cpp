bb1d582ed9d1fda4f0729eb70003d975017180c9
Berkus <berkus@gmail.com>
2013-11-13 11:05:49 +0200

    shell_session compiles.

--- tools/shell/server/shell_session.cpp
+++ tools/shell/server/shell_session.cpp
@@ -1,3 +1,11 @@
+//
+// Part of Metta OS. Check http://metta.exquance.com for latest version.
+//
+// Copyright 2007 - 2013, Stanislav Karchebnyy <berkus@exquance.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 #include <string.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -10,11 +18,16 @@
 #include <signal.h>
 #include <unistd.h>
 #include "shell_session.h"
+#include "logging.h"
+#include "host.h"
+
+using namespace std;
 
 shell_session::shell_session(std::shared_ptr<ssu::stream> stream)
     : shs(stream)
     , ptyfd(-1)
     , ttyfd(-1)
+    , aftty(stream->get_host()->get_io_service())
 {
     logger::debug() << "shell_session";
 
@@ -48,25 +61,25 @@ void shell_session::in_ready()
 
         shell_stream::packet pkt = shs.receive();
         switch (pkt.type) {
-            case shell_stream::Null:
+            case shell_stream::packet_type::Null:
                 if (shs.at_end()) {
                     logger::debug() << "End of remote input";
-                    ::shutdown(aftty.fileDescriptor(), SHUT_WR);
+                    // aftty.close_write();
                 }
                 return; // nothing more to receive for now
 
-            case shell_stream::Data:
+            case shell_stream::packet_type::Data:
                 logger::debug() << this << "input:" << pkt.data;
                 if (!ttyopen) {
-                    error(tr("Received shell data before command to start shell"));
+                    error("Received shell data before command to start shell");
                     break;
                 }
                 if (aftty.write(pkt.data) < 0)
-                    error(aftty.errorString());
+                    error(aftty.error_string());
                 break;
 
-            case shell_stream::Control:
-                got_control(pkt.data);
+            case shell_stream::packet_type::Control:
+                got_control_packet(pkt.data);
                 break;
         }
     }
@@ -74,8 +87,7 @@ void shell_session::in_ready()
 
 void shell_session::out_ready()
 {
-    logger::debug() << "outReady: openmode " << aftty.openMode();
-    assert(aftty.isOpen());
+    assert(aftty.is_open());
 
     while (true) {
         // XX if (shs.bytesToWrite() >= shellBufferSize) return;
@@ -84,7 +96,7 @@ void shell_session::out_ready()
         int act = aftty.read(buf, sizeof(buf));
         if (act <= 0) {
             if (act < 0)
-                error(aftty.errorString());
+                error(aftty.error_string());
             return;
         }
         logger::debug() << "output: " << byte_array(buf, act);
@@ -101,44 +113,48 @@ void shell_session::out_ready()
     }
 }
 
-void shell_session::got_control(byte_array const& msg)
+void shell_session::got_control_packet(byte_array const& msg)
 {
     assert(msg.size() > 0);
 
-    XdrStream rxs(msg);
-    quint32 cmd;
-    rxs >> cmd;
+    byte_array_iwrap<flurry::iarchive> read(msg);
+    int32_t cmd;
+    read.archive() >> cmd;
     switch ((Command)cmd) {
-    case Terminal:  openPty(rxs);   break;
-    case Shell: runShell(rxs);  break;
-    case Exec:  doExec(rxs);    break;
-    default:
-        logger::debug() << this << "ignoring unknown control message type"
-            << cmd;
+        case Terminal:  open_pty(read);   break;
+        case Shell: run_shell(read);  break;
+        case Exec:  do_exec(read);    break;
+        default:
+            logger::debug() << "Ignoring unknown control message type " << cmd;
     }
 }
 
 void shell_session::open_pty(byte_array_iwrap<flurry::iarchive>& rxs)
 {
-    if (ptyfd >= 0)
-        return error(tr("Already have a pseudo-terminal"));
-    if (ttyfd >= 0)
-        return error(tr("Already have a remote shell I/O stream"));
+    if (ptyfd >= 0) {
+        return error("Already have a pseudo-terminal");
+    }
+    if (ttyfd >= 0) {
+        return error("Already have a remote shell I/O stream");
+    }
 
     // Decode the rest of the Terminal control message
-    quint32 width, height, xpixels, ypixels;
+    uint32_t width, height, xpixels, ypixels;
     struct termios tios;
-    rxs >> termname >> width >> height >> xpixels >> ypixels;
-    termunpack(rxs, tios);
-    if (rxs.status() != rxs.Ok)
-        return error(tr("Invalid Terminal request"));
+    rxs.archive() >> termname >> width >> height >> xpixels >> ypixels;
+    termunpack(rxs.archive(), tios);
+    // if (rxs.status() != rxs.Ok)
+        // return error("Invalid Terminal request");
 
-    logger::debug() << "terminal" << termname << "window" << width << "x" << height;
+    logger::debug() << "Terminal " << termname << ", window " << width << "x" << height;
 
     ptyfd = posix_openpt(O_RDWR | O_NOCTTY);
     if (ptyfd < 0)
-        return error(tr("Can't create pseudo-terminal: %0")
-                .arg(strerror(errno)));
+    {
+        ostringstream oss;
+        oss << "Can't create pseudo-terminal: " << strerror(errno);
+        return error(oss.str());
+    }
 
     // Set the pty's window size
     struct winsize ws;
@@ -146,45 +162,50 @@ void shell_session::open_pty(byte_array_iwrap<flurry::iarchive>& rxs)
     ws.ws_row = height;
     ws.ws_xpixel = xpixels;
     ws.ws_ypixel = ypixels;
-    if (ioctl(ptyfd, TIOCSWINSZ, &ws) < 0)
-        logger::debug() << this << "Can't set terminal window size";
+    if (ioctl(ptyfd, TIOCSWINSZ, &ws) < 0) {
+        logger::debug() << "Can't set terminal window size";
+    }
 
     // Set the pty's terminal modes
-    if (tcsetattr(ptyfd, TCSANOW, &tios) < 0)
-        logger::debug() << this << "Can't set terminal parameters";
+    if (tcsetattr(ptyfd, TCSANOW, &tios) < 0) {
+        logger::debug() << "Can't set terminal parameters";
+    }
 }
 
-void shell_session::run_shell(byte_array_iwrap<flurry::iarchive>& rxs)
+void shell_session::run_shell(byte_array_iwrap<flurry::iarchive>&)
 {
-    logger::debug() << this << "run shell";
+    logger::debug() << "Run shell";
     run();
 }
 
 void shell_session::do_exec(byte_array_iwrap<flurry::iarchive>& rxs)
 {
-    std::string cmd;
+    string cmd;
     rxs.archive() >> cmd;
-    if (rxs.status() != rxs.Ok)
-        return error(tr("Invalid Exec request"));
+    // if (rxs.status() != rxs.Ok)
+        // return error("Invalid Exec request");
 
     logger::debug() << "Run command " << cmd;
 
     run(cmd);
 }
 
-void shell_session::run(const QString &/*cmd XXX*/)
+void shell_session::run(string const&/*cmd XXX*/)
 {
-    if (ttyfd >= 0)
-        return error(tr("Already have a remote shell running"));
+    if (ttyfd >= 0) {
+        return error("Already have a remote shell running");
+    }
 
     // If we don't have a pseudo-terminal,
     // create a socket pair for the shell's (non-terminal) stdio.
     int childfd = -1;
     if (ptyfd < 0) {
         int fds[2];
-        if (socketpair(AF_UNIX, SOCK_STREAM, 0, fds) < 0)
-            return error(tr("Can't create socket pair: %0")
-                    .arg(strerror(errno)));
+        if (socketpair(AF_UNIX, SOCK_STREAM, 0, fds) < 0) {
+            ostringstream oss;
+            oss << "Can't create socket pair: " << strerror(errno);
+            return error(oss.str());
+        }
         ttyfd = fds[0];
         childfd = fds[1];
     }
@@ -195,8 +216,9 @@ void shell_session::run(const QString &/*cmd XXX*/)
     if (childpid < 0) {
         if (childfd >= 0)
             close(childfd);
-        return error(tr("Can't create child process: %0")
-                .arg(strerror(errno)));
+        ostringstream oss;
+        oss << "Can't create child process: " << strerror(errno);
+        return error(oss.str());
     }
     if (childpid == 0) {
         // We're the child process.
@@ -204,31 +226,41 @@ void shell_session::run(const QString &/*cmd XXX*/)
         // XXX authenticate user, setuid appropriately
 
         if (ptyfd >= 0) {
-            logger::debug() << "setup child ptys";
+            logger::debug() << "Setup child ptys";
 
             // Set up the pseudo-tty for the child
             signal(SIGCHLD, SIG_DFL);
-            if (grantpt(ptyfd) < 0)
-                { perror("Remote shell: grantpt"); exit(1); }
-            if (unlockpt(ptyfd) < 0)
-                { perror("Remote shell: unlockpt"); exit(1); }
+            if (grantpt(ptyfd) < 0) {
+                perror("Remote shell: grantpt");
+                exit(1);
+            }
+            if (unlockpt(ptyfd) < 0) {
+                perror("Remote shell: unlockpt");
+                exit(1);
+            }
 
             // Establish a new session
-            if (setsid() < 0)
-                { perror("Remote shell: setsid"); exit(1); }
+            if (setsid() < 0) {
+                perror("Remote shell: setsid");
+                exit(1);
+            }
 
             // Open the child end of the terminal
             assert(childfd < 0);
             char *ttyname = ptsname(ptyfd);
-            logger::debug() << "child ptsname " << ttyname;
-            if (ttyname == NULL)
-                { perror("Remote shell: ptsname"); exit(1); }
+            logger::debug() << "Child ptsname " << ttyname;
+            if (ttyname == NULL) {
+                perror("Remote shell: ptsname");
+                exit(1);
+            }
             childfd = open(ttyname, O_RDWR);
-            if (childfd < 0)
-                { perror("Remote shell: open tty"); exit(1); }
+            if (childfd < 0) {
+                perror("Remote shell: open tty");
+                exit(1);
+            }
 
             // Set the TERM environment variable appropriately
-            setenv("TERM", termname.toLocal8Bit().data(), 1);
+            setenv("TERM", termname.c_str(), 1);
         }
         assert(childfd >= 0);
 
@@ -236,7 +268,10 @@ void shell_session::run(const QString &/*cmd XXX*/)
         if (dup2(childfd, STDIN_FILENO) < 0 ||
                 dup2(childfd, STDOUT_FILENO) < 0 ||
                 dup2(childfd, STDERR_FILENO) < 0)
-            { perror("Remote shell: dup2"); exit(1); }
+        {
+            perror("Remote shell: dup2");
+            exit(1);
+        }
 
         // Run the shell/command
         logger::debug() << "child: exec";
@@ -247,14 +282,15 @@ void shell_session::run(const QString &/*cmd XXX*/)
 
     // We're the parent process.
     // First close the child's file descriptor, if any.
-    if (childfd >= 0)
+    if (childfd >= 0) {
         close(childfd);
+    }
 
     // Set up for I/O forwarding.
-    aftty.open(ptyfd >= 0 ? ptyfd : ttyfd, QIODevice::ReadWrite);
+    aftty.open(ptyfd >= 0 ? ptyfd : ttyfd, aftty.ReadWrite);
 
     // Watch for the child process's termination.
-    pidw.watchPid(childpid);
+    pidw.watch_pid(childpid);
 
     logger::debug() << "Started shell";
 }
@@ -264,72 +300,72 @@ void shell_session::error(std::string const& str)
     logger::debug() << "Error: " << str;
 
     // XXX send error control message
-
-    this->deleteLater();
+    // this->deleteLater();
 }
 
 void shell_session::child_done()
 {
-    int rc = pidw.exitStatus();
+    int rc = pidw.exit_status();
     logger::debug() << "Child terminated with status " << rc;
 
-    QByteArray cmsg;
-    XdrStream wxs(&cmsg, QIODevice::WriteOnly);
+    byte_array cmsg;
     if (WIFEXITED(rc)) {
         // Regular process termination
-        wxs << (qint32)ExitStatus << (qint32)WEXITSTATUS(rc);
+        byte_array_owrap<flurry::oarchive> write(cmsg);
+        write.archive() << ExitStatus << WEXITSTATUS(rc);
 
     } else if (WIFSIGNALED(rc)) {
-
         // Process terminated by signal
-        QString signame;
+        string signame;
         switch (WTERMSIG(rc)) {
-        case SIGABRT:   signame = "SIGABRT"; break;
-        case SIGALRM:   signame = "SIGALRM"; break;
-        case SIGBUS:    signame = "SIGBUS"; break;
-        case SIGCHLD:   signame = "SIGCHLD"; break;
-        case SIGCONT:   signame = "SIGCONT"; break;
-        case SIGFPE:    signame = "SIGFPE"; break;
-        case SIGHUP:    signame = "SIGHUP"; break;
-        case SIGILL:    signame = "SIGILL"; break;
-        case SIGINT:    signame = "SIGINT"; break;
-        case SIGKILL:   signame = "SIGKILL"; break;
-        case SIGPIPE:   signame = "SIGPIPE"; break;
-        case SIGQUIT:   signame = "SIGQUIT"; break;
-        case SIGSEGV:   signame = "SIGSEGV"; break;
-        case SIGSTOP:   signame = "SIGSTOP"; break;
-        case SIGTERM:   signame = "SIGTERM"; break;
-        case SIGTSTP:   signame = "SIGTSTP"; break;
-        case SIGTTIN:   signame = "SIGTTIN"; break;
-        case SIGTTOU:   signame = "SIGTTOU"; break;
-        case SIGUSR1:   signame = "SIGUSR1"; break;
-        case SIGUSR2:   signame = "SIGUSR2"; break;
+            case SIGABRT:   signame = "SIGABRT"; break;
+            case SIGALRM:   signame = "SIGALRM"; break;
+            case SIGBUS:    signame = "SIGBUS"; break;
+            case SIGCHLD:   signame = "SIGCHLD"; break;
+            case SIGCONT:   signame = "SIGCONT"; break;
+            case SIGFPE:    signame = "SIGFPE"; break;
+            case SIGHUP:    signame = "SIGHUP"; break;
+            case SIGILL:    signame = "SIGILL"; break;
+            case SIGINT:    signame = "SIGINT"; break;
+            case SIGKILL:   signame = "SIGKILL"; break;
+            case SIGPIPE:   signame = "SIGPIPE"; break;
+            case SIGQUIT:   signame = "SIGQUIT"; break;
+            case SIGSEGV:   signame = "SIGSEGV"; break;
+            case SIGSTOP:   signame = "SIGSTOP"; break;
+            case SIGTERM:   signame = "SIGTERM"; break;
+            case SIGTSTP:   signame = "SIGTSTP"; break;
+            case SIGTTIN:   signame = "SIGTTIN"; break;
+            case SIGTTOU:   signame = "SIGTTOU"; break;
+            case SIGUSR1:   signame = "SIGUSR1"; break;
+            case SIGUSR2:   signame = "SIGUSR2"; break;
 #ifdef SIGPOLL
-        case SIGPOLL:   signame = "SIGPOLL"; break;
+            case SIGPOLL:   signame = "SIGPOLL"; break;
 #endif
-        case SIGPROF:   signame = "SIGPROF"; break;
-        case SIGSYS:    signame = "SIGSYS"; break;
-        case SIGTRAP:   signame = "SIGTRAP"; break;
-        case SIGURG:    signame = "SIGURG"; break;
-        case SIGVTALRM: signame = "SIGVTALRM"; break;
-        case SIGXCPU:   signame = "SIGXCPU"; break;
-        case SIGXFSZ:   signame = "SIGXFSZ"; break;
-        default:
-                signame = QString::number(WTERMSIG(rc));
+            case SIGPROF:   signame = "SIGPROF"; break;
+            case SIGSYS:    signame = "SIGSYS"; break;
+            case SIGTRAP:   signame = "SIGTRAP"; break;
+            case SIGURG:    signame = "SIGURG"; break;
+            case SIGVTALRM: signame = "SIGVTALRM"; break;
+            case SIGXCPU:   signame = "SIGXCPU"; break;
+            case SIGXFSZ:   signame = "SIGXFSZ"; break;
+            default:
+                signame = boost::lexical_cast<string>(WTERMSIG(rc));
                 // XX append '@machine-type', like ssh?
                 break;
         }
-        qint32 flags = 0;
+        int32_t flags = 0;
         if (WCOREDUMP(rc))
             flags |= 1;
-        QString errmsg;
-        QString langtag;        // XXX RFC3066 lang tag?
-        wxs << (qint32)ExitSignal << flags << signame
-            << errmsg << langtag;
+        string errmsg;
+        string langtag;        // XXX RFC3066 lang tag?
+        
+        byte_array_owrap<flurry::oarchive> write(cmsg);
+        write.archive() << ExitSignal << flags << signame << errmsg << langtag;
     }
-    if (!cmsg.isEmpty())
-        shs.sendControl(cmsg);
+    if (!cmsg.is_empty()) {
+        shs.send_control(cmsg);
+    }
 
     // Terminate this shell session
-    this->deleteLater();
+    // this->deleteLater();
 }
--- tools/shell/server/shell_session.h
+++ tools/shell/server/shell_session.h
@@ -1,7 +1,18 @@
+//
+// Part of Metta OS. Check http://metta.exquance.com for latest version.
+//
+// Copyright 2007 - 2013, Stanislav Karchebnyy <berkus@exquance.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 #pragma once
 
+#include "shell_protocol.h"
+#include "shell_stream.h"
 #include "pid_watcher.h"
 #include "async_file.h"
+#include "byte_array_wrap.h"
 
 class shell_session : private shell_protocol
 {
@@ -17,7 +28,7 @@ public:
     ~shell_session();
 
 private:
-    void got_control(byte_array const& msg);
+    void got_control_packet(byte_array const& msg);
 
     void open_pty(byte_array_iwrap<flurry::iarchive>& rxs);
     void run_shell(byte_array_iwrap<flurry::iarchive>& rxs);
