0e05698c79f08a19cdd7b80c137866a9ef916111
Berkus <berkus@gmail.com>
2014-01-21 16:44:51 +0200

    Add substream testing fixture and a test with multiple substreams.

--- tests/test_substreams.cpp
+++ tests/test_substreams.cpp
@@ -11,14 +11,43 @@
 
 #include "simulator_fixture.h"
 
-BOOST_FIXTURE_TEST_CASE(one_substream, simulator_fixture)
+using namespace std;
+using namespace ssu;
+
+struct substreams_fixture : public simulator_fixture
+{
+    std::vector<std::shared_ptr<ssu::stream>> streams; // to keep streams alive
+    std::vector<byte_array> received;
+
+    void stream_setup_listening(std::shared_ptr<ssu::stream> new_stream)
+    {
+        new_stream->set_child_receive_buffer_size(16384);
+        new_stream->listen(ssu::stream::buffer_limit);
+        new_stream->on_new_substream.connect([this, new_stream] {
+            while (auto new_substream = new_stream->accept_substream()) {
+                new_substream->on_ready_read_record.connect([this, new_substream] {
+                    received.emplace_back(new_substream->read_record());
+                });
+                stream_setup_listening(new_substream);
+            }
+        });
+        streams.emplace_back(new_stream);
+    }
+};
+
+// Test stream with one, two and three levels on substreams.
+//
+// stream                         sends nothing
+// +--substream                   sends ONE!
+
+BOOST_FIXTURE_TEST_CASE(one_substream, substreams_fixture)
 {
     simulator->run();
 }
 
 // Test stream with one, two and three levels on substreams.
 //
-// stream                         sends nothing then QUIT! when done
+// stream                         sends nothing
 // +--substream                   sends ONE!
 // +--substream                   sends TWO!
 //    +--subsubstream             sends TWO.ONE!
@@ -26,7 +55,7 @@ BOOST_FIXTURE_TEST_CASE(one_substream, simulator_fixture)
 //    +--subsubstream             sends THREE.ONE!
 //       +--subsubsubstream       sends THREE.ONE.TWO!
 
-BOOST_FIXTURE_TEST_CASE(multiple_substreams, simulator_fixture)
+BOOST_FIXTURE_TEST_CASE(multiple_substreams, substreams_fixture)
 {
     // volatile bool running = true;
     // send_text(stream s, std::string t) { while(running) strand_.post([this](s) { s->write_record(t); })}
@@ -35,20 +64,44 @@ BOOST_FIXTURE_TEST_CASE(multiple_substreams, simulator_fixture)
     // running = false;
     // all_threads.join();
 
+    server->on_new_connection.connect([&] {
+        auto new_stream = server->accept(); // First incoming stream from client
+        stream_setup_listening(new_stream);
+    });
+
     client->connect_to(server_host_eid, "simulator", "test", server_host_address);
 
     auto substream1 = client->open_substream();
-    substream1->write_record("ONE!");
+    substream1->set_priority(1); // lowest
     auto substream2 = client->open_substream();
-    substream2->write_record("TWO!");
+    substream2->set_priority(2);
     auto substream21 = substream2->open_substream();
-    substream21->write_record("TWO.ONE!");
+    substream21->set_priority(4);
     auto substream3 = client->open_substream();
-    substream3->write_record("THREE!");
+    substream1->set_priority(3);
     auto substream31 = substream3->open_substream();
-    substream31->write_record("THREE.ONE!");
+    substream31->set_priority(5);
     auto substream312 = substream31->open_substream();
-    substream312->write_record("THREE.ONE.TWO!");
+    substream312->set_priority(6); // highest
+
+    substream1->write_record("ONE!"); // Should arrive last!
+    substream2->write_record("TWO!");
+    substream21->write_record("TWO.ONE!");
+    substream3->write_record("THREE!");
+    substream31->write_record("THREE.ONE!");
+    substream312->write_record("THREE.ONE.TWO!"); // Should arrive first!
 
     simulator->run();
+
+    for (auto b : received) {
+        logger::debug() << b.as_string();
+    }
+
+    BOOST_CHECK(received[0].as_string() == "THREE.ONE.TWO!");
+    BOOST_CHECK(received[1].as_string() == "THREE.ONE!");
+    BOOST_CHECK(received[2].as_string() == "THREE!");
+    BOOST_CHECK(received[3].as_string() == "TWO.ONE!");
+    BOOST_CHECK(received[4].as_string() == "TWO!");
+    BOOST_CHECK(received[5].as_string() == "ONE!");
 }
+
