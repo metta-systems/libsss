af735e07c17f4f68fbe7ba1414b7c220998d496c
Berkus <berkus@gmail.com>
2014-03-07 16:56:36 +0200

    Fix coding style.

--- lib/base_stream.cpp
+++ lib/base_stream.cpp
@@ -264,13 +264,15 @@ void base_stream::recalculate_receive_window()
     // This should only ever be an issue if we shrink the receive window abruptly,
     // leaving gaps in a formerly-large window.
     // (It's best just to avoid shrinking the receive window abruptly.)
-    if (rx_available_ == 0 and rx_buffer_used_ > 0)
+    if (rx_available_ == 0 and rx_buffer_used_ > 0) {
         rwin = max(rwin, min_receive_buffer_size);
+    }
 
     // Calculate the conservative receive window exponent
     int i = 0;
-    while (size_t((2 << i) - 1) <= rwin)
+    while (size_t((2 << i) - 1) <= rwin) {
         i++;
+    }
     receive_window_byte_ = i;
 
     logger::debug() << "Buffered "
@@ -289,8 +291,9 @@ void base_stream::recalculate_transmit_window(uint8_t window_byte)
     logger::debug() << "Transmit window change " << dec << old_window << "->" << tx_window_
         << ", in use " << tx_inflight_;
 
-    if (tx_window_ > old_window)
+    if (tx_window_ > old_window) {
         tx_enqueue_channel(/*immediate:*/true);
+    }
 }
 
 void base_stream::connect_to(string const& service, string const& protocol)
