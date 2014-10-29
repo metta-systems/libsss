4f9673bc2bb2b89f09c524903ae77dbb6bc743ab
Berkus Decker <berkus@madfire.net>
2014-10-29 12:45:37 +0200

    Add missing #pragma once.

--- include/sss/simulation/sim_connection.h
+++ include/sss/simulation/sim_connection.h
@@ -6,6 +6,8 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+#pragma once
+
 #include "comm/socket_endpoint.h"
 #include "sss/timer.h"
 
