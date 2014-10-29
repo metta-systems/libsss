7d9abc0a9eb63af4564841ff63f2fef107b43bc8
Berkus Decker <berkus@madfire.net>
2014-10-29 12:44:45 +0200

    Move packet magic to channel protocol layer.

--- include/comm/host_interface.h
+++ include/comm/host_interface.h
@@ -11,20 +11,8 @@
 namespace uia {
 namespace comm {
 
-// Probably not in the host_interface.h, rather somewhere in protocol.h
-namespace magic {
-const std::string hello    = "hellopkt";
-const std::string cookie   = "cookipkt";
-const std::string initiate = "init-pkt";
-const std::string message  = "messagep";
-}
-
 class socket;
 class socket_receiver;
-
-// bind_receiver(magic::hello, kex_responder)
-// bind_receiver(magic::initiate, kex_responder)
-// bind_receiver(magic::cookie, kex_initiator)
 
 // Interface used by socket layer to work with the host state.
 // Must be implemented by real host implementation, for example the one in sss.
@@ -35,7 +23,10 @@ public:
     virtual void activate_socket(socket*) = 0;
     virtual void deactivate_socket(socket*) = 0;
 
-    // Interface to bind and lookup receivers based on packet magic value.
+    /// Interface to bind and lookup receivers based on packet magic value.
+    // bind_receiver(magic::hello, kex_responder)
+    // bind_receiver(magic::initiate, kex_responder)
+    // bind_receiver(magic::cookie, kex_initiator)
     virtual void bind_receiver(std::string, std::weak_ptr<socket_receiver>) = 0;
     virtual void unbind_receiver(std::string) = 0;
     virtual bool has_receiver_for(std::string) = 0;
