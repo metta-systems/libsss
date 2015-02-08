c3a90d8d96a303f4f8c074239e90412dadcdc178
Berkus Decker <berkus@madfire.net>
2015-02-08 13:48:31 +0200

    Update code snippet into a real app.

--- doc/snippets.cpp
+++ doc/snippets.cpp
@@ -1,9 +1,16 @@
+#include "sss/host.h"
+#include "sss/server.h"
+#include "sss/stream.h"
+
+int main()
+{
 //! [Creating a host]
-shared_ptr<host> my_host(make_shared<host>());
+host::ptr my_host(host::create());
 //! [Creating a host]
+}
 
 //! [Accepting a connection]
-void some_accept_func(shared_ptr<sss::server> server)
+void some_accept_func(sss::server::ptr server)
 {
     while (auto stream = server->accept()) {
         // Do something with a new stream.
