48fd9a3c358190970b8677f4024f52f56ff78bcb
Berkus Decker <berkus@madfire.net>
2015-09-18 08:09:11 +0300

    Add missing include guards.

--- include/sss/framing/framing.h
+++ include/sss/framing/framing.h
@@ -6,6 +6,8 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+#pragma once
+
 #include "packet_frame.h"
 #include "frame_format.h"
 
