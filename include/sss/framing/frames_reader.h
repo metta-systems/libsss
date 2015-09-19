4b5e4511599fd27fcb5e670775df8e2d9109cf84
Berkus Decker <berkus@madfire.net>
2015-09-19 15:41:00 +0300

    Throw out old channel framing code, use deframe().

--- include/sss/framing/frames_reader.h
+++ include/sss/framing/frames_reader.h
@@ -21,23 +21,24 @@ class frames_reader
 public:
     frames_reader(std::string const& str)
         : buf_(str.data(), str.size())
-    {}
+    {
+    }
 
     frames_reader(boost::asio::const_buffer buf)
         : buf_(std::move(buf))
-    {}
+    {
+    }
 
     void read_packet_header()
     {
         sss::framing::packet_header hdr;
-        std::tie(hdr, buf_) = fusionary::read(hdr, buf_);
+        buf_ = fusionary::read(hdr, buf_);
 
-        std::cout << "Protocol version " << std::showbase << std::hex << hdr.version.value() << std::endl
-                  << "Packet sequence  " << std::showbase << std::hex << hdr.packet_sequence.value() << std::endl;
+        std::cout << "Protocol version " << std::showbase << std::hex << hdr.version.value()
+                  << std::endl
+                  << "Packet sequence  " << std::showbase << std::hex << hdr.packet_sequence.value()
+                  << std::endl;
     }
 
-    void read_frame_header()
-    {
-    }
+    void read_frame_header() {}
 };
-
